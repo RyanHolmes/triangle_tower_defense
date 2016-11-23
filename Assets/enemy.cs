d726acefc63741a06d392413c3878edb8b97656e
Ryan Holmes <ryankaz94@gmail.com>
2016-11-23 08:43:50 -0500

    follows track

---
+++ Assets/enemy.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class enemy : MonoBehaviour {
+
+	public List<Vector3> markers;
+	public int currentTarget = 1;
+	public float speed = 10000000f;//TODO doesnt work at all
+
+	// Use this for initialization
+	void Start () {
+		populateMarks ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 dir = markers [currentTarget] - markers [currentTarget - 1];
+		moveEnemy (dir);
+	}
+
+	void populateMarks(){
+		GameObject[] marks = GameObject.FindGameObjectsWithTag ("mark");
+		List<string> temp = new List<string> (); 
+		for(int j = 0; j < marks.Length; j++){
+			temp.Add (marks[j].name);
+		}
+		temp.Sort ();
+		for(int i = 0; i < temp.Count; i++){
+			for(int j = 0; j < marks.Length; j++){
+				if(temp[i] == marks[j].name){
+					markers.Add (marks[j].transform.position);
+				}
+			}
+		}
+	}
+
+	public void moveEnemy(Vector3 dir){
+		if (!(currentTarget < markers.Count - 1)) {
+			Destroy (this.gameObject);
+		}
+		if (Vector3.Distance (transform.position, markers [currentTarget]) >= 0.1f) {
+			transform.Translate (dir.normalized * speed * Time.deltaTime, Space.World);
+		} else {
+			currentTarget += 1;
+		}
+	}
+}
--- Assets/main.cs
+++ Assets/main.cs
@@ -1,42 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 
 public class main : MonoBehaviour {
 
-	public GameObject enemy;
 	public GameObject start;
 	public GameObject e;
-	public List<Vector3> markers;
-	public int currentTarget = 0;
-//	public float speed = 0.001f;
+	public GameObject enemy;
+	public int currentWave;
+	public int currentEnemy;
 
 	// Use this for initialization
 	void Start () {
-		GameObject[] marks = GameObject.FindGameObjectsWithTag ("mark");
-		List<string> temp = new List<string> (); 
-		for(int j = 0; j < marks.Length; j++){
-			temp.Add (marks[j].name);
-		}
-		temp.Sort ();
-		for(int i = 0; i < temp.Count; i++){
-			for(int j = 0; j < marks.Length; j++){
-				if(temp[i] == marks[j].name){
-					markers.Add (marks[j].transform.position);
-				}
-			}
-		}
-		e = (GameObject)Instantiate (enemy, markers[0], Quaternion.identity);
-//		Vector3 dir = markers [1] - markers [0];
-//		e.transform.Translate (dir.normalized * Time.deltaTime * 10, Space.World);
+		e = (GameObject)Instantiate (enemy, start.transform.position, Quaternion.identity);
+		e.name = "enemy";
+		e.tag = "enemy";
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(e.transform.position.y > markers[1].y){
-			Vector3 dir = markers [1] - markers [0];
-			e.transform.Translate (dir * Time.deltaTime, Space.World);
-		} 
+	void Update () { 
 	}
 }
